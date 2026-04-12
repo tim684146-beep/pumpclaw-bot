14610a7fd281fc02db1904cdeaeb3f7f901162d3
tim684146-beep <tim684146@gmail.com>
2026-04-13 00:25:30 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,10 +8,10 @@ from datetime import datetime
 # CONFIG - À MODIFIER
 # =============================================
 DISCORD_WEBHOOK_URL = "https://discordapp.com/api/webhooks/1493001312449593364/nBZ2Wu2ljp0o-FY9Twfui2ykn2y-4ub8JQDgZoFU7jk5leoYQpD-015XDWUnFlM05NGM"
-MIN_VOLUME_5M = 5000       # Volume minimum 5min en $
-MIN_VOLUME_1H = 20000      # Volume minimum 1h en $
-MIN_MARKET_CAP = 10000     # Market cap minimum en $
-MAX_MARKET_CAP = 500000    # Market cap maximum en $
+MIN_VOLUME_5M = 1000       # Volume minimum 5min en $
+MIN_VOLUME_1H = 5000      # Volume minimum 1h en $
+MIN_MARKET_CAP = 5000     # Market cap minimum en $
+MAX_MARKET_CAP = 1000000    # Market cap maximum en $
 CHECK_INTERVAL = 120       # Vérification toutes les 2 minutes
 # =============================================
 
